c19447624185700cde3c7e491d080a37de432d7f
prashantkumar1 <prashant.1.kumar@gmail.com>
2015-09-03 09:59:10 +0530

    Twitter login integrated with login form
    Login form is integrated with twitter login button ,
    on the click of it twitter account details will be automatically
    transfered to login form

---
+++ app/src/main/java/com/example/myultra/registration/MainActivity.java
@@ -0,0 +1,219 @@
+package com.example.myultra.registration;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+
+import android.content.Intent;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    // TWITTER LOGIN KEYS
+    private static final String TWITTER_KEY = "Aym8wPz3KUzyF3xv6o2O37ZWc";
+    private static final String TWITTER_SECRET = "OgQzuorwIFphoHwMMHfrrWakvpwzdoUQvjXT8IzkM0aPH9rpDm";
+    private TwitterLoginButton loginButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+        setContentView(R.layout.activity_main);
+
+ //INITIALIZATION OF BUTTONS
+        Button button;
+        final EditText edit_name, edit_pass, edit_email, edit_phone;
+        final CheckBox check;
+        SharedPreferences pref;
+        final SharedPreferences.Editor editor;
+
+
+
+
+
+//INSTANCES
+        button = (Button) findViewById(R.id.button);
+        edit_name = (EditText) findViewById(R.id.edit_name);
+        edit_pass = (EditText) findViewById(R.id.edit_pass);
+        edit_email = (EditText) findViewById(R.id.edit_email);
+        edit_phone = (EditText) findViewById(R.id.edit_phone);
+        check = (CheckBox) MainActivity.this.findViewById(R.id.checkBox);
+        pref = getSharedPreferences("Registration", 0);
+        editor = pref.edit();
+
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // Do something with result, which provides a TwitterSession for making API calls
+                Toast.makeText(getApplicationContext(),"LOGIN SUCCESSFUL ",Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(MainActivity.this,upload_photo.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+
+                Toast.makeText(getApplicationContext(),"Back to Login ",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+    //CHECKBOX FUNTION
+
+    check.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            if(!check.isChecked())
+            {
+                edit_pass.setTransformationMethod(new PasswordTransformationMethod());
+            }
+            else
+            {
+                edit_pass.setTransformationMethod(null);
+            }
+        }
+    });
+
+
+
+    //BUTTONS
+
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            boolean flag =true;
+            String name=edit_name.getText().toString();
+            String email=edit_email.getText().toString();
+            String pass=edit_pass.getText().toString();
+            String phone=edit_phone.getText().toString();
+
+
+            if(!isValidEmail(email))
+            {
+                edit_email.setError("Invalid Email");
+                flag=false;
+            }
+            if(!isValidPassword(pass))
+            {
+                edit_pass.setError("Invalid Password");
+                flag=false;
+            }
+            if (!isValidPhone(phone))
+            {
+                edit_phone.setError("Invalid phone");
+                flag=false;
+            }
+
+
+
+            if(flag==true)
+            {
+                Toast.makeText(getApplicationContext(), "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                editor.putString("Name", name);
+                editor.putString("Email", email);
+                editor.putString("phone",phone);
+                editor.putString("password",pass);
+                editor.commit();
+                Intent i = new Intent(MainActivity.this,upload_photo.class);
+                startActivity(i);
+
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(),"LOGIN UNSUCCESSFUL",Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
+
+}
+
+
+    //FUNCTIONS
+
+    private boolean isValidEmail(String email)
+    {
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        Pattern pattern=Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher=pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    private boolean isValidPassword(String password)
+    {
+        if(password!=null && password.length()>6)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    private boolean isValidPhone(String phone)
+    {
+        if (phone.length()==10)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/example/myultra/registration/upload_photo.java
@@ -0,0 +1,119 @@
+package com.example.myultra.registration;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+public class upload_photo extends ActionBarActivity {
+
+
+
+    ImageView iv;
+    Button b;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_photo);
+
+        iv = (ImageView) findViewById(R.id.imageView);
+        b = (Button) findViewById(R.id.button4);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+    }
+
+
+    private void selectImage() {
+
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery", "Cancel" };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo")) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, 1);
+                }
+
+                else if (options[item].equals("Choose from Gallery")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+
+                }
+
+                else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                Bitmap bp = (Bitmap) data.getExtras().get("data");
+                iv.setImageBitmap(bp);
+            }
+            else if (requestCode == 2) {
+
+                Uri selectedImage = data.getData();
+                String[] filePath = {MediaStore.Images.Media.DATA};
+                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                Log.w("path of image from gallery......******************.........", picturePath + "");
+                iv.setImageBitmap(thumbnail);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_upload_photo, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
